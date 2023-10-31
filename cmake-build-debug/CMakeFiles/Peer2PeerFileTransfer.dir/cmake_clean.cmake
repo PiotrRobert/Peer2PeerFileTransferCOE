ae40c3147152126e8e459dde3ad56a32cb1d431a
peter <peter.szczepaniec@gmail.com>
2023-10-31 15:04:54 -0400

    Test1

---
+++ cmake-build-debug/CMakeFiles/Peer2PeerFileTransfer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Peer2PeerFileTransfer.dir/main.c.obj"
+  "Peer2PeerFileTransfer.exe"
+  "Peer2PeerFileTransfer.exe.manifest"
+  "Peer2PeerFileTransfer.pdb"
+  "libPeer2PeerFileTransfer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Peer2PeerFileTransfer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Peer2PeerFileTransfer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Peer2PeerFileTransfer.
+# This may be replaced when dependencies are built.
